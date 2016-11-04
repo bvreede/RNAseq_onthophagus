de0ab406a1ca300868429999adbd89280d52eaf6
Barbara Vreede <b.vreede@gmail.com>
2016-11-04 12:30:08 -0400

    added other options for fixing problematic tsv

--- fixtsv.py
+++ fixtsv.py
@@ -1,12 +1,13 @@
 
 import os
+#import pandas as pd
 sourcedir = "/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Onthophagus_FoldChange"
-
 
 """
 for f in os.listdir(sourcedir):
 	if ".annot.tsv" not in f:
 		continue
+	
 	infile = open("%s/%s" %(sourcedir,f))
 	outfile = open("%s/%s" %(sourcedir,f.replace(".tsv",".csv")), "w")
 	count=0
@@ -20,8 +21,8 @@ for f in os.listdir(sourcedir):
 		count +=1
 	outfile.close()
 	print "number of lines in file %s:" %f, count
-
-	
+"""
+"""	
 infile = open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/OnthophagusFPKM.annot.tsv")
 outfile = open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/gene_annotation_table.csv", "w")
 
@@ -39,7 +40,8 @@ for line in infile:
 outfile.close()
 print "total no. rows:", count
 """
-	
+
+"""	
 outfile = open("/Users/BarbaraMaria/Box Sync/PROJECT doublesex/Scripts/github-barbara/onthophagus/to_R.txt","w")
 for f in os.listdir(sourcedir):
 	if ".annot.csv" not in f:
@@ -50,3 +52,57 @@ for f in os.listdir(sourcedir):
 	outfile.write('res <- res[complete.cases(res$padj),]\n')
 	outfile.write('res_sig <- res[res$padj <= 0.05,]\n')
 	outfile.write('dim(res_sig)\n')
+"""
+
+#gofile = pd.DataFrame.from_csv("/Users/BarbaraMaria/Box Sync/Onthophagus/OtauGenomicData/otau_blastP_uniprot_GO.txt", sep="\t")
+#gofile.to_csv("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Otau-genedata/godata2.tsv", sep='\t')
+#print gofile["Gene.ontology..molecular.function."][371:377]
+
+def problemfixer(l):
+	l = l.replace("5'","5pr")
+	l = l.replace("3'","3pr")
+	l = l.replace("2'","2pr")
+	#l = l.replace("-PA","")
+	#l = l.replace("; ","+")
+	#l = l.replace(", ","+")
+	l=l.replace("'","") #Jerdon's jumping ant, Anna's something or other
+	return l
+"""
+goin= open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Otau-genedata/godata2.tsv")
+goout = open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Otau-genedata/godata3.tsv","w")
+count = 0
+dbdict = {}
+for line in goin:
+	count +=1
+	lili = line.split('\t')
+	dbdict[count] = lili
+	for l in lili[:-1]:
+		l = problemfixer(l)
+		goout.write("%s\t"%l.strip())
+	lastl = problemfixer(lili[-1].strip())
+	goout.write("%s\n" %lastl)
+
+goout.close()
+print dbdict[10214]
+"""
+
+"""
+genein = open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Otau-genedata/Otau_genedata.tsv")
+geneout = open("/Users/BarbaraMaria/Dropbox/projects/2016_dsx-phase2/data/Otau-genedata/Otau_genedata2.tsv","w")
+
+count = 0
+dbdict = {}
+for line in genein:
+	count +=1
+	lili = line.split('\t')
+	dbdict[count] = lili
+	for l in lili[:-1]:
+		l = problemfixer(l)
+		geneout.write("%s\t"%l.strip())
+	lastl = problemfixer(lili[-1].strip())
+	geneout.write("%s\n" %lastl)
+
+geneout.close()
+"""
+
+
