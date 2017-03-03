853d6f165ea96d7662a94242b2d6ec29ea7bdff7
Bioinformatics <bioinformatics@129-79-227-176.dhcp-bl.indiana.edu>
2017-03-03 09:39:37 -0500

    adjusted folder locations

--- tophat_batch_launcher.sh
+++ tophat_batch_launcher.sh
@@ -5,9 +5,9 @@ PATH=$PATH:/Users/bioinformatics/bin/bowtie2-2.2.5
 export PATH
 
 tophatdir=/Users/bioinformatics/bin/tophat-2.0.14.OSX_x86_64
-indexdir=/Volumes/HD1/otau/Onthophagus_Tophat_Index
-outputdir=/Volumes/HD2/dsx_RNAseq_phase2/tophat_alignments
-inputdir=/Volumes/HD2/dsx_RNAseq_phase2/trimmedreads
+indexdir=/Volumes/HD1v2/otau/Onthophagus_Tophat_Index
+outputdir=/Volumes/HD3v2/barbara/tophat_alignments
+inputdir=/Volumes/HD3v2/barbara/trimmedreads
 
 cd $inputdir
 
