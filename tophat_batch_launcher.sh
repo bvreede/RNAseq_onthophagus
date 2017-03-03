19c5f51e055cf4f8bc75ee5a4bd57ddbb6bb8014
Bioinformatics <bioinformatics@129-79-227-176.dhcp-bl.indiana.edu>
2017-03-03 12:02:02 -0500

    edited folder destinations

--- tophat_batch_launcher.sh
+++ tophat_batch_launcher.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #batch tophat alignment script
 
-PATH=$PATH:/Users/bioinformatics/bin/bowtie2-2.2.5
+#add bowtie and samtools to path
+PATH=$PATH:/Users/bioinformatics/bin/bowtie2-2.2.5:/Users/bioinformatics/bin/samtools-1.3.1
 export PATH
 
 tophatdir=/Users/bioinformatics/bin/tophat-2.0.14.OSX_x86_64
@@ -12,8 +13,8 @@ inputdir=/Volumes/HD3v2/barbara/trimmedreads
 cd $inputdir
 
 for file in *; do
-echo "Starting alignment of $file"
+echo "Starting alignment of $file..."
 $tophatdir/tophat2 -o $outputdir/$file --transcriptome-index=$indexdir/transcriptome-index -p8 --b2-very-sensitive --read-edit-dist 2 $indexdir/Otaur.scaffolds $file
-echo "Finished aligning $file!"
+echo "Alignment of $file complete."
 
 done
