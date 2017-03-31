9f36e274321d841405beab92d6684d1e1c5abe0e
Barbara Vreede <b.vreede@gmail.com>
2017-03-31 06:16:10 -0400

    two small females

--- htseq_batch_launcher.sh
+++ htseq_batch_launcher.sh
@@ -9,11 +9,11 @@ export PATH
 tophatdir=/Users/bioinformatics/bin/tophat-2.0.14.OSX_x86_64
 gff=/Volumes/HD1v2/otau/Onthophagus_Tophat_Index/OTAU.Models.gff3
 outputdir=/Volumes/HD3v2/barbara/htseq_counts
-inputdir=/Volumes/HD3v2/barbara/tophat_alignments
+inputdir=/Volumes/HD3v2/barbara/tophat_alignments_smallF
 
 cd $inputdir
 
-for file in small_female*; do
+for file in *; do
 
 echo "Starting count analysis for $file..."
 
