55590db38207b05aea5c24a6476f9ce69edd3ce5
Barbara Vreede <b.vreede@gmail.com>
2017-03-31 03:45:27 -0400

    two small F samples

--- tophat_batch_launcher.sh
+++ tophat_batch_launcher.sh
@@ -7,12 +7,12 @@ export PATH
 
 tophatdir=/Users/bioinformatics/bin/tophat-2.0.14.OSX_x86_64
 indexdir=/Volumes/HD1v2/otau/Onthophagus_Tophat_Index
-outputdir=/Volumes/HD3v2/barbara/tophat_alignments
-inputdir=/Volumes/HD3v2/barbara/trimmedreads
+outputdir=/Volumes/HD3v2/barbara/tophat_alignments_smallF
+inputdir=/Volumes/HD1v2/dsxRNAseq/barbara/trimmedreads_smallF
 
 cd $inputdir
 
-for file in small_female_*; do
+for file in *; do
 echo "Starting alignment of $file..."
 $tophatdir/tophat2 -o $outputdir/$file --transcriptome-index=$indexdir/transcriptome-index -p8 --b2-very-sensitive --read-edit-dist 2 $indexdir/Otaur.scaffolds $file
 echo "Alignment of $file complete."
